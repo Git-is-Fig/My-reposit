1d40f9ddb97945fa71a2523cb67bfe9c71bcd98d
Hohlov Kostiantyn <lookop231@gmail.com>
2023-05-22 15:29:38 +0300

    First

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        int n = 40;
+
+        int a = 1;
+        int b = 1;
+
+        Console.WriteLine("Перші {0} чисел Фібоначчі:", n);
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine(a);
+
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+
+        Console.ReadLine();
+    }
+}
+
